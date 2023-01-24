5707b8a02f61346d7d0c8b169aaf3657e5ac803f
Tarctic <85291498+Tarctic@users.noreply.github.com>
2023-01-24 05:24:00 +0000

    Completed section 7 testing

--- challenges/testing/begin/main_test.go
+++ challenges/testing/begin/main_test.go
@@ -1,8 +1,72 @@
 // challenges/testing/begin/main_test.go
 package main
 
+import "testing"
+
 // write a test for letterCounter.count
+func TestLetterCounter(t *testing.T) {
+	tests := []struct{
+		name string
+		input string
+		want int
+	}{
+		{input: "#00", want: 0},
+		{input: "a2_32_^_&/)", want: 1},
+		{input: "812_%6ab//", want: 2},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			got := letterCounter{}.count(tc.input)
+			if got != tc.want {
+				t.Errorf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
 
 // write a test for numberCounter.count
+func TestNumberCounter(t *testing.T) {
+	tests := []struct{
+		name string
+		input string
+		want int
+	}{
+		{input: "#00", want: 2},
+		{input: "abc_1,?/", want: 1},
+		{input: "abc_12&8_^", want: 3},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			got := numberCounter{}.count(tc.input)
+			if got != tc.want {
+				t.Errorf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+
 
 // write a test for symbolCounter.count
+func TestSymbolCounter(t *testing.T) {
+	tests := []struct{
+		name string
+		input string
+		want int
+	}{
+		{input: "#00", want: 1},
+		{input: "abc_1,?/", want: 4},
+		{input: "abc_12&8_^_", want: 5},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			got := symbolCounter{}.count(tc.input)
+			if got != tc.want {
+				t.Errorf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
--- testing/table-driven/begin/main_test.go
+++ testing/table-driven/begin/main_test.go
@@ -1,19 +1,28 @@
 // testing/table-driven/begin/main_test.go
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	// var tests = []struct {
-	// 	name  string
-	// 	input []int
-	// 	want  int
-	// }{
-	// 	{"one", []int{1}, 1},
-	// 	{"two", []int{1, -2}, -1},
-	// 	{"three", []int{1, 2, 3}, 6},
-	// }
+	var tests = []struct {
+		name string
+		input []int
+		want int
+	} {
+		{"one", []int{1}, 1},
+		{"two", []int{1,-3}, -2},
+		{"three", []int{1,2,3}, 7},
+	}
 
 	// range over the tests and run them as subtests
-	//
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T){
+			got := sum(tc.input...)
+			if got != tc.want {
+				t.Errorf("got %v, wanted %v",got, tc.want)
+			}
+		})
+	}
 }
